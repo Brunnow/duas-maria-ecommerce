714b6f7eb11db45f99dd3be4997dbdc44300b9bc
Bruno Oliveira <brunk1809@gmail.com>
2026-05-18 08:47:09 -0300

    adding categories

--- src/main/java/com/ecommerce/duas_marias/controlller/CategoryController.java
+++ src/main/java/com/ecommerce/duas_marias/controlller/CategoryController.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ecommerce.duas_marias.model.Category;
@@ -17,6 +19,11 @@ public class CategoryController {
     @GetMapping("/api/public/categories")
     public List<Category> getAllCategories() {
         return categories;
+    }
+    @PostMapping("/api/public/categories")
+    public String createCategory(@RequestBody Category category) {
+        categories.add(category);
+        return "Create category";
     }
 
 }
