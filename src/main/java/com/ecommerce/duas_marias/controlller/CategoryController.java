911508668ec9ebe561833242065a44ec2e6c4839
Bruno Oliveira <brunk1809@gmail.com>
2026-05-20 23:13:02 -0300

    delete category

--- src/main/java/com/ecommerce/duas_marias/controlller/CategoryController.java
+++ src/main/java/com/ecommerce/duas_marias/controlller/CategoryController.java
@@ -6,7 +6,9 @@ import java.util.List;
 import com.ecommerce.duas_marias.service.CategoryService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,5 +35,11 @@ public class CategoryController {
         categoryService.createCategory(category);
         return "Create category";
     }
+
+    @DeleteMapping("/api/admin/categories/{categoryId}")
+    public String deleteCategory(@PathVariable Long categoryId) {
+        String status = categoryService.deleteCategory(categoryId);
+        return status;
+    }   
 
 }
