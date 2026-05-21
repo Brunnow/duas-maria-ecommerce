c16c17785fa2220e6e45cccf5cb23b876e6281c8
Bruno Oliveira <brunk1809@gmail.com>
2026-05-21 07:50:43 -0300

    Beginning to implement ResponseEntity

--- src/main/java/com/ecommerce/duas_marias/controlller/CategoryController.java
+++ src/main/java/com/ecommerce/duas_marias/controlller/CategoryController.java
@@ -6,12 +6,15 @@ import java.util.List;
 import com.ecommerce.duas_marias.service.CategoryService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.ecommerce.duas_marias.model.Category;
 
@@ -37,9 +40,14 @@ public class CategoryController {
     }
 
     @DeleteMapping("/api/admin/categories/{categoryId}")
-    public String deleteCategory(@PathVariable Long categoryId) {
-        String status = categoryService.deleteCategory(categoryId);
-        return status;
+    public ResponseEntity<String> deleteCategory(@PathVariable Long categoryId) {
+        try {
+            String status = categoryService.deleteCategory(categoryId);
+            return new ResponseEntity<>(status, HttpStatus.OK);
+        } catch (ResponseStatusException e) {
+            return new ResponseEntity<>(e.getReason(), e.getStatusCode());
+        }    
+        
     }   
 
 }
--- src/main/java/com/ecommerce/duas_marias/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/duas_marias/service/CategoryServiceImpl.java
@@ -1,7 +1,10 @@
 package com.ecommerce.duas_marias.service;
 
 import com.ecommerce.duas_marias.model.Category;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,7 +31,7 @@ public class CategoryServiceImpl implements CategoryService{
         Category category = categories.stream()
                 .filter(c -> c.getCategoryId().equals(categoryId))
                 .findFirst()
-                .orElse(null);
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource not found"));
         if (category == null) {
             return "Category not found.";
         }
