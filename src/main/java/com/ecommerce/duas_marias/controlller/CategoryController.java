175c64bce8c748811aa8339688dff807bb72e3b7
Igor Leonardo <igorleonnard@gmail.com>
2026-05-22 03:00:58 -0300

    Update Category created

--- src/main/java/com/ecommerce/duas_marias/controlller/CategoryController.java
+++ src/main/java/com/ecommerce/duas_marias/controlller/CategoryController.java
@@ -8,12 +8,7 @@ import com.ecommerce.duas_marias.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.ecommerce.duas_marias.model.Category;
@@ -51,6 +46,19 @@ public class CategoryController {
             return new ResponseEntity<>(e.getReason(), e.getStatusCode());
         }    
         
-    }   
+    }
+
+    @PutMapping("/api/public/categories/{categoryId}")
+    public ResponseEntity<String> updateCategory(@RequestBody Category category,
+                                                 @PathVariable Long categoryId){
+        try {
+            Category savedCategory = categoryService.updateCategory(category, categoryId);
+            return new ResponseEntity<>("Category with category id = " + categoryId, HttpStatus.OK);
+
+        } catch (ResponseStatusException e) {
+            return new ResponseEntity<>(e.getReason(), e.getStatusCode());
+        }
+
+    }
 
 }
--- src/main/java/com/ecommerce/duas_marias/service/CategoryService.java
+++ src/main/java/com/ecommerce/duas_marias/service/CategoryService.java
@@ -12,4 +12,5 @@ public interface CategoryService {
 
     String deleteCategory(Long categoryId);
 
+    Category updateCategory(Category category, Long categoryId);
 }
