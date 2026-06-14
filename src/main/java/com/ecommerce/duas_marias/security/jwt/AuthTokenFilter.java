36e80eb7911b626d42bbfc2aa51e019d71b696a4
Bruno Oliveira <brunk1809@gmail.com>
2026-06-14 16:49:41 -0300

    Loose ends

--- src/main/java/com/ecommerce/duas_marias/security/jwt/AuthTokenFilter.java
+++ src/main/java/com/ecommerce/duas_marias/security/jwt/AuthTokenFilter.java
@@ -1,5 +1,6 @@
 package com.ecommerce.duas_marias.security.jwt;
 
+import com.ecommerce.duas_marias.security.services.UserDetailsServiceImpl;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -23,7 +24,7 @@ public class AuthTokenFilter extends OncePerRequestFilter {
     private JwtUtils jwtUtils;
 
     @Autowired
-    private UserDetailsService userDetailsService;
+    private UserDetailsServiceImpl userDetailsService;
 
     private static final Logger logger = LoggerFactory.getLogger(AuthTokenFilter.class);
 
