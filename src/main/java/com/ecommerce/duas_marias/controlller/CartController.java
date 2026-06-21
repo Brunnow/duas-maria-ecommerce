4c0891abf1e45d8c75e4e5f3e58dd524c9e2916e
Bruno Oliveira <brunk1809@gmail.com>
2026-06-21 20:30:40 -0300

    Fetching Users Cart

--- src/main/java/com/ecommerce/duas_marias/controlller/CartController.java
+++ src/main/java/com/ecommerce/duas_marias/controlller/CartController.java
@@ -1,7 +1,10 @@
 package com.ecommerce.duas_marias.controlller;
 
+import com.ecommerce.duas_marias.model.Cart;
 import com.ecommerce.duas_marias.payload.CartDTO;
+import com.ecommerce.duas_marias.repositories.CartRepository;
 import com.ecommerce.duas_marias.service.CartService;
+import com.ecommerce.duas_marias.util.AuthUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,16 +16,33 @@ import java.util.List;
 @RequestMapping("/api")
 public class CartController {
     @Autowired
+    private CartRepository cartRepository;
+
+    @Autowired
+    private AuthUtil authUtil;
+    @Autowired
     private CartService cartService;
+
     @PostMapping("/carts/products/{productId}/quantity/{quantity}")
     public ResponseEntity<CartDTO> addProductToCart(@PathVariable Long productId,
-                                                    @PathVariable Integer quantity){
+                                                    @PathVariable Integer quantity) {
         CartDTO cartDTO = cartService.addProductToCart(productId, quantity);
         return new ResponseEntity<CartDTO>(cartDTO, HttpStatus.CREATED);
     }
+
     @GetMapping("/carts")
     public ResponseEntity<List<CartDTO>> getCarts() {
         List<CartDTO> cartDTOs = cartService.getAllCarts();
         return new ResponseEntity<List<CartDTO>>(cartDTOs, HttpStatus.FOUND);
+    }
+
+    @GetMapping("/carts/users/cart")
+    public ResponseEntity<CartDTO> getCartById() {
+        String emailId = authUtil.loggedInEmail();
+        Cart cart = cartRepository.findCartByEmail(emailId);
+        Long cartId = cart.getCartId();
+        CartDTO cartDTO = cartService.getCart(emailId, cartId);
+        return new ResponseEntity<CartDTO>(cartDTO, HttpStatus.OK);
+
     }
 }
