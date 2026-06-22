1d237891e1397b51df7e96a5ce17b5e472900a16
Igor Leonardo <igorleonnard@gmail.com>
2026-06-22 20:50:35 -0300

    Update Product Quantity in the Cart

--- src/main/java/com/ecommerce/duas_marias/controlller/CartController.java
+++ src/main/java/com/ecommerce/duas_marias/controlller/CartController.java
@@ -43,6 +43,14 @@ public class CartController {
         Long cartId = cart.getCartId();
         CartDTO cartDTO = cartService.getCart(emailId, cartId);
         return new ResponseEntity<CartDTO>(cartDTO, HttpStatus.OK);
+    }
 
+    @PutMapping("/cart/products/{productId}/quantity/{operation}")
+    public ResponseEntity<CartDTO> updateCartProduct(@PathVariable Long productId,
+                                                     @PathVariable String operation){
+        CartDTO cartDTO = cartService.updateProductQuantityInCart(productId,
+                operation.equalsIgnoreCase("delete") ? -1 : 1);
+
+        return new ResponseEntity<CartDTO>(cartDTO, HttpStatus.OK);
     }
 }
